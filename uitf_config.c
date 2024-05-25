3998f2347a3a890b5aabb311fdc33308c42cc7f5
Bryan Moffit <moffit@jlab.org>
2024-05-24 21:16:29 -0400

    initial commit of config routines

---
+++ uitf_config.c
@@ -0,0 +1,20 @@
+/*************************************************************************
+ *
+ *  uitf_config.c - Library of routines to read configuration parameters
+ *                  for the modules in the MOTT DAQ
+ *
+ *
+ *
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <libconfig.h>
+
+int32_t
+uitf_config_parse(char *config_filename)
+{
+
+
+  return 0;
+}
